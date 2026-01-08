0c70ced4e0f82b6f539e23b7d31725a2ab81dd11
Azeggouar <k.azeggouar@gmail.com>
2026-01-08 15:28:13 +0100

    ecom

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,7 @@
+@extends('Master_page')
+@section('title','Home')
+@section('content')
+<h1 class="text-center">Bienvenue sur notre site e-commerce</h1>
+<p class="text-center">Découvrez nos produits de qualité à des prix compétitifs</p>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,59 @@ Route::get('/hello', function () {
 Route::get('/hello1', function () {
     return view('hello');
 });
+
+
+Route::get('/produits/{cat}', function ($cat) {
+    $produits=[];
+
+	if($cat=='hicking'){
+	$produits=array(
+                    array(
+                         "nom" => "Sac à dos",
+                         "prix"=>200,
+                         "image" => "sac_do.jfif"
+                          ),
+                     array(
+                         "nom" => "tente",
+                         "prix"=>300,
+                         "image" => "tent.jfif"
+                          ),
+                      array(
+                         "nom" => "Montre GPS",
+                         "prix"=>150,
+                         "image" => "watch_gps.jfif"
+                          )
+                 );
+
+
+
+	}
+	else if($cat=='electromenager')
+	{
+	$produits=array(
+                    array(
+                         "nom" => "Machine à laver",
+                         "prix"=>3000,
+                         "image" => "machine_lav.jfif"
+                          ),
+                     array(
+                         "nom" => "Four",
+                         "prix"=>1500,
+                         "image" => "four.jfif"
+                          ),
+                      array(
+                         "nom" => "Micro onde",
+                         "prix"=>1000,
+                         "image" => "micro_onde.jfif"
+                          )
+                 );
+
+	}
+
+
+	return view('Produits', [
+        'products' => $produits,
+        'categorie' => $cat
+    ]);
+});
+
