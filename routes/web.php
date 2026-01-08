697fa3d0d5f3ffdf8e477463973506ced03ab0a7
Azeggouar <k.azeggouar@gmail.com>
2026-01-08 14:38:20 +0100

    add hello route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response('<h1>Hello from vercel</h1>');
+});
